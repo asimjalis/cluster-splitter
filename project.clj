69f5d01777a5dbc236317a28852aa7c2565a1259
Asim Jalis <asimjalis@gmail.com>
2014-11-07 07:20:24 -0800

    Renaming to use hypen and code cleanup

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cluster_splitter "1.1.0-SNAPSHOT"
+(defproject cluster-splitter "1.1.0-SNAPSHOT"
   :description "Splits IP addresses into small training clusters."
   :dependencies [
     [org.clojure/clojurescript "0.0-2371"]
@@ -7,18 +7,19 @@
     ]
   :plugins [
     [lein-cljsbuild "1.0.3"]
-    [com.cemerick/clojurescript.test "0.3.1"] ]
+    [com.cemerick/clojurescript.test "0.3.1"] 
+    ]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {
     :builds {
       :dev
       {:source-paths ["src-cljs"]
-       :compiler {:output-to "cluster_splitter.js"
+       :compiler {:output-to "cluster-splitter.js"
                   :optimizations :whitespace
                   :pretty-print true}}
       :prod
       {:source-paths ["src-cljs"]
-       :compiler {:output-to "cluster_splitter.js"
+       :compiler {:output-to "cluster-splitter.js"
                   :optimizations :advanced
                   :externs  [ "js/jquery-1.9.1-externs.js" ]
                   :pretty-print true}}}})
--- src-cljs/cluster_splitter.cljs
+++ src-cljs/cluster_splitter.cljs
@@ -1,4 +1,4 @@
-(ns cluster_splitter
+(ns cluster-splitter
   (:use
     [clojure.string :only [join]]
     [clojure.walk :only [keywordize-keys]]
@@ -14,10 +14,12 @@
       :refer (is deftest with-test run-tests testing test-var)]
       ))
 
-(defn-let foo [a]
-  x a
-  y (+ x 10)
-  z (+ y 100))
+;[Todo]
+; Add tests for the ip->machine->cluster->str process
+; Rename machine-seq->4-or-5-cluster-seq 
+; Publish on real endpoint
+; Use - in name instead of _
+; Add [json-html "0.2.3"] (use 'json-html.core) https://github.com/yogthos/json-html
 
 (defn debug-with-repl []
   (repl/connect "http://localhost:9000/repl"))
@@ -34,11 +36,12 @@
     (keywordize-keys) 
     (apply merge)))
 
-(deftest test-something
-  (is (= 3 3)))
+(defn ^:export reload []
+  (.reload js/location))
+  
 
-(defn run-tests []
-  (t/test-ns 'cluster_splitter))
+(defn ^:export run-tests []
+  (t/test-ns 'cluster-splitter))
 
 (defn str->int 
   "Convert string to int or return default."
@@ -46,59 +49,82 @@
   (let [x (js/parseInt x)] 
     (if (js/isNaN x) default x)))
 
-(deftest ^:export test-str->int
-  (is (= 10 (str->int 4 "10")))
-  (is (= -1 (str->int 4 "-1")))
-  (is (=  0 (str->int 4 "0")))
-  (is (=  4 (str->int 4 "hello")))
-  (is (=  4 (str->int 4 "")))
-  (is (=  1 (str->int 4 "1.0")))
-  (is (=  0 (str->int 4 "0.9")))
-  (is (=  0 (str->int 4 "0.001"))))
+(defn-test 
+  str->int 
+  10 [4 "10"] 
+  -1 [4 "-1"] 
+  0  [4 "0"]
+  4  [4 "hello"]
+  4  [4 ""]
+  1  [4 "1.0"]
+  1  [4 "1.9"]
+  1  [4 "1.001"])
   
 (defn html->output [html] 
   (.html (js/$ "#output") html))
 
-(defn str->ips
+(defn str->ip-seq
   "Extract IP addresses from text and return as vector." 
   [s]
   (->> s (re-seq #"\d+\.\d+\.\d+\.\d+")))
 
-(defn clusters->str [clusters]
-  (->> clusters 
+(defn-test str->ip-seq
+  ["1.1.1.1"]           ["hi 1.1.1.1 random text 2.2.2"]
+  ["1.1.1.1" "2.2.2.2"] ["hi 1.1.1.1 random text 2.2.2.2"])
+
+(defn cluster-seq->str [cluster-seq]
+  (->> cluster-seq 
     (map (fn [pairs] (->> pairs (map #(join "\t" %)) (join "\n")))) 
     (map-indexed #(str "Cluster " (inc %) "\n" %2)) (join "\n\n")))
 
-(defn ips->4-cluster-str [ips]
-  (->> ips 
-    (partition 2) (map vec) 
-    (partition 4) (map vec)
-    clusters->str))
+(defn-test cluster-seq->str 
+  (str "Cluster 1\n" "a\tb") 
+    [ [ [ ["a" "b"] ] ] ]
+  (str "Cluster 1\n" "a\tb\n" "c\td") 
+    [ [ [ ["a" "b"] ["c" "d"] ] ] ]
+  (str "Cluster 1\n" "a\tb" "\n\n" "Cluster 2\n" "c\td") 
+    [ [ [ ["a" "b"] ] [ ["c" "d"] ] ] ]
+  )
 
-(defn merge-cluster-lists [cluster-list1 cluster-list2]
-  (map #(into %1 %2) cluster-list1 cluster-list2))
+(defn ip-seq->machine-seq [ip-seq]
+  (->> ip-seq (partition 2) (map vec)))
 
-(defn-let ips->5-cluster-str [ips]
-  ip-count     (count ips)
-  med-ip-count (/ ip-count 5)
-  med-cluster-list (->> ips 
-                     (take med-ip-count) 
-                     (partition 2) (map vec)
-                     (partition 1) (map vec))
-  sml-cluster-list (->> ips 
-                     (drop med-ip-count) 
-                     (partition 2) (map vec)
-                     (partition 4) (map vec))
-  clusters (merge-cluster-lists med-cluster-list sml-cluster-list)
-  cluster-str (->> clusters clusters->str))
+(defn machine-seq->cluster-seq [machines-per-cluster machine-seq]
+  (->> machine-seq (partition machines-per-cluster) (map vec)))
+
+(defn merge-cluster-seqs [seq1 seq2]
+  (map #(into %1 %2) seq1 seq2))
+
+(defn machine-seq->4-cluster-seq [machine-seq]
+  (->> machine-seq (machine-seq->cluster-seq 4)))
+
+(defn-let machine-seq->5-cluster-seq [machine-seq]
+  machine-count (count machine-seq)
+  large-machine-count (/ machine-count 5)
+  large-cluster-seq (->> machine-seq (take large-machine-count) (machine-seq->cluster-seq 1))
+  small-cluster-seq (->> machine-seq (drop large-machine-count) (machine-seq->cluster-seq 4))
+  cluster-seq (merge-cluster-seqs large-cluster-seq small-cluster-seq))
+
+(defn machine-seq->4-or-5-cluster-seq [machines-per-cluster machine-seq]
+  (if (= 5 machines-per-cluster) 
+     (machine-seq->5-cluster-seq machine-seq)
+     (machine-seq->4-cluster-seq machine-seq)))
+
+(defn-test machine-seq->4-or-5-cluster-seq
+  [ [ :a :b :c :d ]                       ] [ 4 [ :a :b :c :d ] ]
+  [ [ :a :b :c :d ]       [ :e :f :g :h ] ] [ 4 [ :a :b :c :d :e :f :g :h ] ]
+  [ [ :x :a :b :c :d ]                    ] [ 5 [ :x    :a :b :c :d ] ]
+  [ [ :x :a :b :c :d ] [ :y :e :f :g :h ] ] [ 5 [ :x :y :a :b :c :d :e :f :g :h ] ]
+  )
 
 (defn-let input-map->cluster-html [input-map]
-  cluster-size (->> input-map :clusterSize (str->int 4))
-  ips          (->> input-map :ipText str->ips)
-  cluster-str  (if (= 5 cluster-size) 
-                 (ips->5-cluster-str ips)
-                 (ips->4-cluster-str ips))
-  cluster-html (str "<pre>" cluster-str "</pre>"))
+  machines-per-cluster  (->> input-map :clusterSize (str->int 4))
+  ip-seq                (->> input-map :ipText str->ip-seq)
+  machine-seq           (->> ip-seq ip-seq->machine-seq)
+  cluster-seq           (->> machine-seq 
+                          (machine-seq->4-or-5-cluster-seq machines-per-cluster))
+  cluster-seq-str       (->> cluster-seq cluster-seq->str)
+  cluster-html          (str "<pre>" cluster-seq-str "</pre>"))
 
 (defn ^:export main []
   (->> (form->map) 
